c6e8ed73071be3d8d3bfbd73510262aa873680ae
Timothé Lauvernier <timothe@lauvernier.fr>
2023-01-29 18:27:19 +0100

    first step express and embedding comments

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end("Voici la première étape");
-});
-
-server.listen(process.env.Port || 3000);
+const http = require("http"); // require permet d'importer modules NODE
+const app = require("./app"); // Appel le fichier app.js
+app.set("port", process.env.Port || 3000); // Défini quel port utiliser
+const server = http.createServer(app);
+server.listen(process.env.Port || 3000); // Ecoute le serveur sur le port 3000 ou alors sur le port par défault de la plateforme
