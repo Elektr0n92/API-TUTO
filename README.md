c76a7efe6535f46c6184c22262220587db8943ca
Timothé <109433280+Elektr0n92@users.noreply.github.com>
2023-01-30 01:48:42 +0100

    Upgrade server, middleware and correction comment/readme file
    * Add middleware and fonction next
    
    * Add fonction normalizeport and errorHandler
    
    * modify comment
    
    * Modify readme file

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Entrainement API
 
-## TIPS NODEMON 
+## TIPS NODEMON
+
 For use nodemon with powershell, put with strategic editor of windows 'activate running script' on 'Active'
+
+## Run frontend
+
+For run frontend environment you must use terminal with command : npm run start
+
+## Run server
+
+For run server you must use terminal command : node server or nodemon (watch)
--- server.js
+++ server.js
@@ -1,5 +1,51 @@
 const http = require("http"); // require permet d'importer modules NODE
-const app = require("./app"); // Appel le fichier app.js
-app.set("port", process.env.Port || 3000); // Défini quel port utiliser
+const app = require("./app"); // Appel le fichier app.js (pas besoin d'ajouter .js avec require)
+
+// La fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+/* La fonction errorHandler recherche les différentes erreurs et les gère de manière appropriée. 
+Elle est ensuite enregistrée dans le serveur */
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
 const server = http.createServer(app);
-server.listen(process.env.Port || 3000); // Ecoute le serveur sur le port 3000 ou alors sur le port par défault de la plateforme
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+
+const port = normalizePort(process.env.PORT || "3000"); // Ecoute le serveur sur le port 3000 ou alors sur le port par défault de la plateforme
+app.set("port", port); // Défini quel port utiliser
+server.listen(port);
