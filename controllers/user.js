59a70475f6d39d610c0d328e5660a0ef77cd9ca5
Timothé Lauvernier <timothe@lauvernier.fr>
2023-02-03 13:48:35 +0100

    ADD logic controller login

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,8 @@
-const bcript = require("bcrypt"); // permet de crypter les mots de passes
+const bcrypt = require("bcrypt"); // permet de crypter les mots de passes
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
-  bcript
+  bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
       const user = new User({
@@ -19,4 +19,31 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Paire login/mot de passe incorrecte" });
+      }
+      /*La méthode compare de bcrypt compare un string avec un hash pour, par exemple, 
+      vérifier si un mot de passe entré par l'utilisateur correspond à un hash sécurisé enregistré en base de données. 
+      Cela montre que même bcrypt ne peut pas décrypter ses propres hashs. */
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: "Paire login/mot de passe incorrecte" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: "TOKEN",
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
